33197c6deddd66990f470acfec5923a5ae480687
Emir Albayrak <61053198+T4WW@users.noreply.github.com>
2020-12-06 23:00:33 +0300

    Create discordBot.py

---
+++ discordBot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+import deemix
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():	
+	
+	guild_count = 0
+	for guild in bot.guilds:		
+		print(f"- {guild.id} (name: {guild.name})")
+		guild_count = guild_count + 1
+	print("SampleDiscordBot is in " + str(guild_count) + " guilds.")
+
+@bot.command()
+async def test(ctx, arg):
+    pass
+
+
+bot.run("NzgyNzAzMDkwMjQzMDc2MTE3.X8QDDg.EHXAQz085ZlF_7iLGU6LLLlOv_Y")
